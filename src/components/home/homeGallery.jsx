a7e8d95c00b97cad9fd22d9ed59c6aa5b25df0a0
ceemoods <carl@ramani.io>
2023-01-11 16:38:18 -0500

    Fix home gallery broken link.

--- src/components/home/homeGallery.jsx
+++ src/components/home/homeGallery.jsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useStateContext } from "../../backend/contextAPI";
 
 const HomeGallery = ({ products }) => {
-  const {  addLinkId } = useStateContext();
-   
     return (
     <div className="home-gallery">
       <h1>Favourites</h1>
       {products.slice(0, 3).map((product) => (
         <div key={product.id} className="gallery-box">
           <p className="gallery-box-title">{product.name}</p>
-          <Link to="/menu" onClick={()=> addLinkId(1)}>
+          <Link to="/menu">
             <img src={product.image} alt="pizza" />
           </Link>
           <p className="gallery-box-story">{product.story}</p>
